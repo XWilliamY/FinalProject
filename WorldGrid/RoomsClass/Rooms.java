d519fd6e527e350960722795f98c3736563dd3dd
Brian Won <brianwontonsoup@gmail.com>
2015-01-08 12:16:31 -0500

    Made folder RoomClass, started Rooms.java, need to make text files of room and item descriptions

---
+++ WorldGrid/RoomsClass/Rooms.java
@@ -0,0 +1,14 @@
+public class Rooms{
+    private int ID, item;
+    private String description;
+
+    public Rooms(){
+    }
+
+    public void setID(int x){
+	ID = x;
+    }
+
+    public void setDes(){
+	
+}
